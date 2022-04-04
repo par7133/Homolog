5c185cf4a1673880c28d57cfe2aadc3eac8f8929
Daniele Bonini (皮夕): WebDev and DevOps by lots of Sim.pli.city bits <my25mb@aol.com>
2022-04-04 23:13:11 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -603,7 +603,7 @@
   <meta name="author" content="5 Mode"/> 
   <meta name="robots" content="index,follow"/>
   
-  <script src="/HL_js/jquery-3.1.0.min.js" type="text/javascript"></script>
+  <script src="/HL_js/jquery-3.6.0.min.js" type="text/javascript"></script>
   <script src="/HL_js/common.js" type="text/javascript"></script>
   <script src="/HL_js/bootstrap.min.js" type="text/javascript"></script>
   
